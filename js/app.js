6721e2a8c1472459dc9c0ace116c00c7df989001
nirajpandkar <niraj.pandkar@gmail.com>
2017-06-27 20:41:56 +0530

    (feat) Filter function implemented

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
-var marker;
 var markers=[];
+var marker;
 var map;
 var bounds;
 var infoWindow;
@@ -19,6 +19,9 @@ function initMap(){
     }
 
     map.fitBounds(bounds);
+
+
+
 }
 
 // Create an array of markers using places array.
@@ -36,15 +39,52 @@ var addMarker = function(place){
 
     // Push marker into markers array
     markers.push(marker);
-
+    //self.markersArray().push([marker]);
     // Extend the map boundaries to encompass all the markers
     bounds.extend(marker.position);
 
     // Onclick event listener to open infoWindow at each marker.
     marker.addListener('click', function() {
         fillInfoWindow(this, infoWindow);
+        toggleBounce(this);
     });
 };
+
+// not required to be removed
+document.getElementById('show-listing').addEventListener('click', showMarkers);
+document.getElementById('hide-listing').addEventListener('click', function(){
+    hideMarkers(markers);
+});
+
+
+function showMarkers(){
+    for(var i=0; i<markers.length; i++){
+
+
+        markers[i].setMap(map);
+        bounds.extend(markers[i].position);
+
+    }
+    //map.fitBounds(bounds);
+}
+
+function hideMarkers(markers){
+    for(var i=0; i<markers.length; i++){
+        markers[i].setMap(null);
+    }
+}
+
+function toggleBounce(marker) {
+
+    for(var i=0;i<markers.length;i++){
+        markers[i].setAnimation(null);
+    }
+    if(marker){
+        marker.setAnimation(google.maps.Animation.BOUNCE);
+    }
+
+
+}
 
 // populate the infowindow with appropriate information
 function fillInfoWindow(marker, infoWindow){
@@ -62,12 +102,30 @@ function fillInfoWindow(marker, infoWindow){
 }
 
 
-//var ViewModel = function(){
-//    var self = this;
-//    self.markers = ko.observableArray([]);
-//
-//
-//};
-//
-//ko.applyBindings(new ViewModel());
+var ViewModel = function(){
+    var self = this;
+    self.markersArray = ko.observableArray(places);
 
+    self.searchString = ko.observable();
+    self.searchResults = ko.computed(function() {
+        var string = self.searchString();
+        if(string == null){
+            showMarkers();
+            return places;
+        }
+        else{
+            hideMarkers(markers);
+            return ko.utils.arrayFilter(places, function(place) {
+                if(place.title.toLowerCase().indexOf(string.toLowerCase()) >= 0) {
+                    addMarker(place);
+                    return place;
+                }
+            });
+        }
+    });
+
+
+};
+
+
+ko.applyBindings(ViewModel);
